8bb957d2740f0e21df3be5446c4c6d52b00a5974
Aye yes wait a minute MR POSTMANNNNNNNN <XxxSahalXxx@gmail.com>
2017-06-02 15:24:53 -0400

    Add files via upload

---
+++ plan.java
@@ -0,0 +1,40 @@
+
+ /*Import scanners
+ Classes
+
+
+Main method
+
+Each player is given half the deck. Each Player then must turn up a card at the same time and the player with the higher card takes both cards and puts them, 
+face down, 
+on the bottom of his stack. If the cards are the same rank, it is *War*. Each player turns up one card face down and one card face up.
+The player with the higher cards takes both piles .
+
+random arrays 
+
+Method
+ Give each cards value & the suits
+
+
+
+method
+ Make an If statement " If Player A card BIGGER than players B card then Player A gets +1 card 
+                        if two player have drawn the same card then an additional card will be drawn the other card if higher will determine the winner of the war
+
+
+
+
+Method For AI                                       
+
+
+
+
+Determine a winner when a player deck is at 0
+
+
+
+Exit */
+
+
+         
+
